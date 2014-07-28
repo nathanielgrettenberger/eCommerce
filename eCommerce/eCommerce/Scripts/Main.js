b250ade684668d72340c8257107aeb5e9a333f3b
SeedPaths-User <lessdismissive@email.com>
2014-07-28 14:53:51 -0600

    more edits

--- eCommerce/eCommerce/Scripts/Main.js
+++ eCommerce/eCommerce/Scripts/Main.js
@@ -1,13 +1,20 @@
 ﻿
+$(document).ready(function () {
+    $('.thumbnails img').on('click', function () {
+        var thumbImg = $(this).attr('src');
+        $('.productimg').attr('src', thumbImg);
+        $('.thumbnails img').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+//$('.imageView .imageTabs img').on('click', function () {
+
 //when the image is clicked
-
-$('.imageView .imageTabs img').on('click', function () {
-
     //do this
     //set a variable to the src value
 
-    var url = $(this).attr('src');
+   
 
     //change the attr: src to the value url
 
-    $('.imageView .mainImage img').attr('src', url)});
+ 
